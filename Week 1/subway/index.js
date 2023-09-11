800734649078f7a8780b54dad1f017fd7af88b9d
Aman Kumar <amankumar7506@gmail.com>
2023-09-11 18:39:11 -0400

    add price module

--- Week 1/subway/index.js
+++ Week 1/subway/index.js
@@ -1,35 +1,6 @@
 import inquirer from 'inquirer';
 import { calculateTotal } from './calculateTotal';
-
-const breadChoices = {
-    'Wheat': 0.50,
-    'White': 0.00,
-    'Pita': 1.00
-};
-
-const meatChoices = {
-    'Roast Beef': 2.50,
-    'Ham': 1.75,
-    'Turkey': 2.00,
-    'Tuna': 3.00
-};
-
-const cheeseChoices = {
-    'Swiss': 1.25,
-    'American': 0.50,
-    'Cheddar': 2.25
-};
-
-const toppingChoices = {
-    'Tomato': 0.25,
-    'Pickles': 0.25,
-    'Onions': 0.25,
-    'Mushrooms': 0.25,
-    'Hot Chili\'s': 0.25,
-    'Green Peppers': 0.25,
-    'Olives': 0.25
-};
-
+import { breadChoices, meatChoices, cheeseChoices, toppingChoices } from './prices';
 
 const main = async () => {
     let total = 0;
---
+++ Week 1/subway/prices.js
@@ -0,0 +1,28 @@
+export const breadChoices = {
+    'Wheat': 0.50,
+    'White': 0.00,
+    'Pita': 1.00
+};
+
+export const meatChoices = {
+    'Roast Beef': 2.50,
+    'Ham': 1.75,
+    'Turkey': 2.00,
+    'Tuna': 3.00
+};
+
+export const cheeseChoices = {
+    'Swiss': 1.25,
+    'American': 0.50,
+    'Cheddar': 2.25
+};
+
+export const toppingChoices = {
+    'Tomato': 0.25,
+    'Pickles': 0.25,
+    'Onions': 0.25,
+    'Mushrooms': 0.25,
+    'Hot Chili\'s': 0.25,
+    'Green Peppers': 0.25,
+    'Olives': 0.25
+};
