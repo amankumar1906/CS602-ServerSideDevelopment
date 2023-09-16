b1e23aa95c187df08194047cf0d83755e248aeb1
Aman Kumar <amankumar7506@gmail.com>
2023-09-16 16:39:54 -0400

    add middleware

---
+++ Week 2/Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+      "id": 1,
+      "name": "Alice Johnson",
+      "email": "alice.johnson@example.com",
+      "status": "active"
+    },
+    {
+      "id": 2,
+      "name": "Bob Smith",
+      "email": "bob.smith@example.com",
+      "status": "inactive"
+    },
+    {
+      "id": 3,
+      "name": "Charlie Brown",
+      "email": "charlie.brown@example.com",
+      "status": "active"
+    }
+  ]
+
+  module.exports = members;
--- Week 2/index.js
+++ Week 2/index.js
@@ -1,10 +1,21 @@
-import express from "express"
+const express = require("express")
+const path = require("path")
+const members = require('./Members.js')
 
 const app = express();
 
-app.get('/', (req,res) => {
-res.send('Hello World!')
+const logger = (req,res,next) => {
+    console.log(`${req.protocol}://${req.get('host')}`);
+    next();
+}
+
+app.use(logger)
+
+app.get('/api/members', (req,res) => {
+    res.json(members)
 })
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
