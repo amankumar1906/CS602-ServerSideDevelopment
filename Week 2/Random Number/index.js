2aa2a5ec19fb6ec7bac28b74ef1e8f74defc840e
Aman Kumar <amankumar7506@gmail.com>
2023-09-16 17:41:22 -0400

    add frontend

--- Week 2/Random Number/index.js
+++ Week 2/Random Number/index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require("path");
+const app = express();
 
+app.get('/random-number', (req, res) => {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    res.json({number: randomNumber});
+});
 
-app.get('/', (req,res) => {
-    const randomNumber = Math.floor(Math.random())
-    res.json({number: randomNumber})
-})
+app.use(express.static(path.join(__dirname, 'public')));
 
-PORT = 3000
-app.listen(PORT, () => console.log(`Server running on ${PORT}`))
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
