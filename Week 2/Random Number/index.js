6a41590f479453dfdfedfebe37fa6d8f02a36588
Aman Kumar <amankumar7506@gmail.com>
2023-09-17 16:05:51 -0400

    update middleware

--- Week 2/Random Number/index.js
+++ Week 2/Random Number/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const path = require("path");
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/random-number', (req, res) => {
     const randomNumber = Math.floor(Math.random() * 100) + 1;
     res.json({number: randomNumber});
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
