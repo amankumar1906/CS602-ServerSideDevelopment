855f8746e8c6a5777f35df517e173f686f8441f7
Aman Kumar <amankumar7506@gmail.com>
2023-09-18 19:33:24 -0400

    add formatting

--- Week 2/Random Number/index.js
+++ Week 2/Random Number/index.js
@@ -1,14 +1,13 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/random-number', (req, res) => {
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    res.json({number: randomNumber});
+app.get("/random-number", (req, res) => {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  res.json({ number: randomNumber });
 });
-
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
